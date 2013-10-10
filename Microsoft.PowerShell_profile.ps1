73fac3dd7b955feae2cfab628c93251c6b1b7f11
Brian G. Shacklett <bshacklett@penbayhealthcare.org>
2013-10-10 10:03:40 -0400

    Initial Repo Creation

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,6 @@
+﻿# Load posh-git example profile
+. 'C:\Users\bshacklett\Documents\WindowsPowerShell\posh-git\profile.example.ps1'
+
+# Load posh-hg example profile
+. 'C:\Users\bshacklett\Documents\WindowsPowerShell\posh-hg\profile.example.ps1'
+
