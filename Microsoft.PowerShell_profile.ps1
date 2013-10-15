e739ef0ad02dab843d9cb57f991cfc0bbf6ac60a
Brian G. Shacklett <bshacklett@penbayhealthcare.org>
2013-10-15 10:54:29 -0400

    Using envar to source Posh-Git and Posh-HG modules.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 ﻿# Load posh-git example profile
-. 'C:\Users\bshacklett\Documents\WindowsPowerShell\posh-git\profile.example.ps1'
+. "$HOME\My Documents\WindowsPowerShell\posh-git\profile.example.ps1"
 
 # Load posh-hg example profile
-. 'C:\Users\bshacklett\Documents\WindowsPowerShell\posh-hg\profile.example.ps1'
+. "$HOME\My Documents\WindowsPowerShell\posh-hg\profile.example.ps1"
 
