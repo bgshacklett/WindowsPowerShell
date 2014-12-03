e584cc2595e349cfc366ef1062442048de13385c
Brian Shacklett <brian.shacklett@globaloptions.com>
2014-12-03 12:37:43 -0500

    Updated posh-git; Removing posh-hg

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,9 +5,6 @@ $userprofile = $env:USERPROFILE
 # Load posh-git example profile
 . "$userprofile\Documents\WindowsPowerShell\posh-git\profile.example.ps1"
 
-# Load posh-hg example profile
-. "$userprofile\Documents\WindowsPowerShell\posh-hg\profile.example.ps1"
-
 # Load "Stash" module.
 import-module Stash
 
