61ed5e4669ecbc364205fa58547e01315a28ed80
Brian Shacklett <brian.shacklett@globaloptions.com>
2015-03-20 17:18:01 -0400

    Updated Submodule; Updated profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,14 @@
 ﻿
 $userprofile = $env:USERPROFILE
 
+If ($env:HOMESHARE) {
+
+    # Set and force overwrite of the $HOME variable
+    Set-Variable -Name HOME -Value $env:HOMESHARE -Force
+
+    # Set the "~" shortcut value for the FileSystem provider
+    (get-psprovider 'FileSystem').Home = $env:HOMESHARE
+}
 
 # Load posh-git example profile
 . "$userprofile\Documents\WindowsPowerShell\profile.posh-git.ps1"
