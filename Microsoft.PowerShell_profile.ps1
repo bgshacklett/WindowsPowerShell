1d10d83a1e53126dfcb003b204717d78c304fb1d
Brian Shacklett <brian.shacklett@rackspace.com>
2017-08-20 19:14:43 +0000

    Config path for python3; modify prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -57,21 +57,23 @@ $env:NODE_PATH = "${NodeModulesCustomPath}:${env:NODE_PATH}"
 # ################################
 $customPathEntries =
 @(
-  $env:PATH                             # System Defined Path
-  $(Get-VimPath)                        # Vim
-  "$UserPrograms\NeoVim\bin"            # NeoVim
-  "$UserPrograms\GNU\DiffUtils\bin"     # DiffUtils
-  "$UserPrograms\HashiCorp\Packer"      # Packer
-  "$UserPrograms\Rackspace\faws-cli"    # FAWS CLI
-  "$UserPrograms\Rackspace\Maestro"     # Maestro
-  "$UserPrograms\JMESPath\jp"           # JP from the JMESPath Project
-  "${env:ProgramFiles(x86)}\Nmap"       # NMAP
-  "C:\Chocolatey\Bin"                   # Packages installed by Chocolatey
-  "$env:NPM_PACKAGES"                   # NPM Packages
-  "$Env:APPDATA\npm"                    # Global NPM Modules
-  "$Env:HOME\go\bin"                    # Go binaries
-  "C:\Users\bria0265\node_modules\.bin" # Node binaries
-  "C:\MinGW\msys\1.0\bin"               # MSYS Binaries
+  $env:PATH                              # System Defined Path
+  $(Get-VimPath)                         # Vim
+  "$UserPrograms\NeoVim\bin"             # NeoVim
+  "$UserPrograms\GNU\DiffUtils\bin"      # DiffUtils
+  "$UserPrograms\HashiCorp\Packer"       # Packer
+  "$UserPrograms\Rackspace\faws-cli"     # FAWS CLI
+  "$UserPrograms\Rackspace\Maestro"      # Maestro
+  "$UserPrograms\JMESPath\jp"            # JP from the JMESPath Project
+  "${env:ProgramFiles(x86)}\Nmap"        # NMAP
+  "C:\Chocolatey\Bin"                    # Packages installed by Chocolatey
+  "C:\Chocolatey\lib\jq.1.5\tools"       # JQ in the Chocolatey folder
+  "$env:NPM_PACKAGES"                    # NPM Packages
+  "$Env:APPDATA\npm"                     # Global NPM Modules
+  "$Env:HOME\go\bin"                     # Go binaries
+  "C:\Users\bria0265\node_modules\.bin"  # Node binaries
+  "C:\MinGW\msys\1.0\bin"                # MSYS Binaries
+  "$Env:APPDATA\Python\Python35\Scripts" # Python3 Scripts
 )
 #
 # Set $env:PATH
@@ -121,6 +123,7 @@ Start-SshAgent
 
 # Configure the Prompt
 $GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true
+$GitPromptSettings.DefaultPromptSuffix = '[$(Get-Fawsenvironment)] $(''>'' * ($nestedPromptLevel + 1)) '
 
 # Load PSReadline module.
 try
